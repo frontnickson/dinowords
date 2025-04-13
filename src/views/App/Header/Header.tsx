a30100d86e5ec456384fc2ca57e5ef229e6c4129
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-04-13 11:59:37 +0300

    get header

--- src/views/App/Header/Header.tsx
+++ src/views/App/Header/Header.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from '../../../data/store/store';
-import { removeUser } from '../../../data/slices/userSlice';
+import React, {useState} from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../../../data/store/store';
+import {removeUser} from '../../../data/slices/userSlice';
 
 import logo from '../../images/logo.svg'
 import burgerMenu from '../../images/burger-menu.svg'
@@ -17,124 +17,135 @@ import styles from './Header.module.scss'
 
 const Header: React.FC = () => {
 
-  const dispatch = useDispatch()
-  const [menu, setMenu] = useState<boolean>(false)
-  const [opactiy, setOpactiy] = useState({ opacity: 0 })
-  const user = useSelector((state: RootState) => state.user.token)
+    const dispatch = useDispatch()
+    const [menu, setMenu] = useState<boolean>(false)
+    const [opactiy, setOpactiy] = useState({opacity: 0})
+    const user = useSelector((state: RootState) => state.user.token)
 
-  const handleClick = () => {
-    setOpactiy({ opacity: 0 });
+    const handleClick = () => {
+        setOpactiy({opacity: 0});
 
-    setTimeout(() => {
-      setOpactiy({ opacity: 1 });
-    }, 100);
-  };
+        setTimeout(() => {
+            setOpactiy({opacity: 1});
+        }, 100);
+    };
 
-  const handleClose = () => {
-    setOpactiy({ opacity: 1 });
+    const handleClose = () => {
+        setOpactiy({opacity: 1});
 
-    setTimeout(() => {
-      setOpactiy({ opacity: 0 })
-    }, 100)
+        setTimeout(() => {
+            setOpactiy({opacity: 0})
+        }, 100)
 
-    setTimeout(() => {
-      setMenu(false)
-    }, 200)
-  }
+        setTimeout(() => {
+            setMenu(false)
+        }, 200)
+    }
 
-  const handleExitProfile = () => {
-    dispatch(removeUser())
-    window.location.href = "/about"
-  }
+    const handleExitProfile = () => {
+        dispatch(removeUser())
+        window.location.href = "/about"
+    }
 
-  return (
-    <header className={styles.header}>
+    return (
+        <header className={styles.header}>
+            <div className={styles.content}>
 
-      {user ? (
-        <div className={styles.content}>
-
-          <Link to="/about" style={{ display: "flex", alignItems: "center" }}>
-            <img src={logo} alt='logo' className={styles.content_logo} />
-          </Link>
-
-          <div className={styles.content_con}>
-
-            <nav className={styles.content_conNav}>
-              <ul className={styles.content_conList}>
-
-                <NavLink to="/about">
-                  <li><p className={styles.content_conListItem}><img src={fireIcon} alt='words' />О сервисе</p></li>
-                </NavLink>
-
-                <NavLink to="/words">
-                  <li><p className={styles.content_conListItem}><img src={wordsIcon} alt='words' />Таблица слов</p></li>
-                </NavLink>
-
-                <NavLink to="/progress">
-                  <li><p className={styles.content_conListItem}><img src={progressIcon} alt='progress' />Изученные слова</p></li>
-                </NavLink>
-
-                <NavLink to="/profile">
-                  <li><p className={styles.content_conListItem}><img src={profileIcon} alt='prfile' />Профиль</p></li>
-                </NavLink>
-
-              </ul>
-            </nav>
-
-            <Link to="/quest">
-              <button className='btn'>Начать практику</button>
-            </Link>
-
-            <img className={styles.content_burger} src={burgerMenu} alt='menu' onClick={() => { setMenu(true); handleClick() }} />
-
-          </div>
-
-          {menu && (
-            <div className={styles.content_menu} style={opactiy}>
-
-              <button className={styles.content_menuClose} onClick={() => { handleClose() }}><img src={closeIcon} alt='close' /></button>
-
-              <ul className={styles.content_menuList}>
-
-                <Link to="/about" onClick={() => { setMenu(false) }}>
-                  <li><p style={{ display: "flex", alignItems: "center", fontSize: "30px" }}><img src={fireIcon} alt='words' />О сервисе</p></li>
+                <Link to="/about" style={{display: "flex", alignItems: "center"}}>
+                    <img src={logo} alt='logo' className={styles.content_logo}/>
                 </Link>
 
-                <Link to="/words" onClick={() => { setMenu(false) }}>
-                  <li><p style={{ display: "flex", alignItems: "center", fontSize: "30px" }}><img src={wordsIcon} alt='words' />Таблица слов</p></li>
-                </Link>
+                <div className={styles.content_con}>
 
-                <Link to="/progress" onClick={() => { setMenu(false) }}>
-                  <li><p style={{ display: "flex", alignItems: "center", fontSize: "30px" }}><img src={progressIcon} alt='words' />Изученные слова</p></li>
-                </Link>
+                    <nav className={styles.content_conNav}>
+                        <ul className={styles.content_conList}>
 
-                <Link to="/profile" onClick={() => { setMenu(false) }}>
-                  <li><p style={{ display: "flex", alignItems: "center", fontSize: "30px" }}><img src={profileIcon} alt='words' style={{ height: "30px" }} />Профиль</p></li>
-                </Link>
+                            <NavLink to="/about">
+                                <li><p className={styles.content_conListItem}><img src={fireIcon} alt='words'/>О сервисе
+                                </p></li>
+                            </NavLink>
 
-              </ul>
-              <Link to="/quest" onClick={() => { setMenu(false) }}>
-                <button className='btn-mobile'>Начать практику</button>
-              </Link>
+                            <NavLink to="/words">
+                                <li><p className={styles.content_conListItem}><img src={wordsIcon} alt='words'/>Таблица
+                                    слов</p></li>
+                            </NavLink>
 
-              <div>
-                <p onClick={handleExitProfile}>Выйти</p>
-              </div>
+                            <NavLink to="/progress">
+                                <li><p className={styles.content_conListItem}><img src={progressIcon} alt='progress'/>Изученные
+                                    слова</p></li>
+                            </NavLink>
+
+                            <NavLink to="/profile">
+                                <li><p className={styles.content_conListItem}><img src={profileIcon} alt='prfile'/>Профиль
+                                </p></li>
+                            </NavLink>
+
+                        </ul>
+                    </nav>
+
+                    <Link to="/quest">
+                        <button className='btn'>Начать практику</button>
+                    </Link>
+
+                    <img className={styles.content_burger} src={burgerMenu} alt='menu' onClick={() => {
+                        setMenu(true);
+                        handleClick()
+                    }}/>
+
+                </div>
+
+                {menu && (
+                    <div className={styles.content_menu} style={opactiy}>
+
+                        <button className={styles.content_menuClose} onClick={() => {
+                            handleClose()
+                        }}><img src={closeIcon} alt='close'/></button>
+
+                        <ul className={styles.content_menuList}>
+
+                            <Link to="/about" onClick={() => {
+                                setMenu(false)
+                            }}>
+                                <li><p style={{display: "flex", alignItems: "center", fontSize: "30px"}}><img
+                                    src={fireIcon} alt='words'/>О сервисе</p></li>
+                            </Link>
+
+                            <Link to="/words" onClick={() => {
+                                setMenu(false)
+                            }}>
+                                <li><p style={{display: "flex", alignItems: "center", fontSize: "30px"}}><img
+                                    src={wordsIcon} alt='words'/>Таблица слов</p></li>
+                            </Link>
+
+                            <Link to="/progress" onClick={() => {
+                                setMenu(false)
+                            }}>
+                                <li><p style={{display: "flex", alignItems: "center", fontSize: "30px"}}><img
+                                    src={progressIcon} alt='words'/>Изученные слова</p></li>
+                            </Link>
+
+                            <Link to="/profile" onClick={() => {
+                                setMenu(false)
+                            }}>
+                                <li><p style={{display: "flex", alignItems: "center", fontSize: "30px"}}><img
+                                    src={profileIcon} alt='words' style={{height: "30px"}}/>Профиль</p></li>
+                            </Link>
+
+                        </ul>
+                        <Link to="/quest" onClick={() => {
+                            setMenu(false)
+                        }}>
+                            <button className='btn-mobile'>Начать практику</button>
+                        </Link>
+
+                        <div>
+                            <p onClick={handleExitProfile}>Выйти</p>
+                        </div>
+                    </div>
+                )}
             </div>
-          )}
-        </div>
-      ) : (
-        <div className={styles.content}>
-
-          <Link to="/about" style={{ display: "flex", alignItems: "center" }}>
-            <img src={logo} alt='logo' className={styles.content_logo} />
-          </Link>
-
-        </div>
-      )}
-
-    </header>
-  );
+        </header>
+    );
 };
 
 export default Header;
