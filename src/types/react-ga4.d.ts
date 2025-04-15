8ebdfc229aae37957f75d378b6b721374fb65ac1
NikitaPutzavr <151025166+NikitaPutzavr@users.noreply.github.com>
2025-04-15 16:35:31 +0300

    fixed ga4

--- src/types/react-ga4.d.ts
+++ src/types/react-ga4.d.ts
@@ -1,12 +1,6 @@
-// declare module 'react-ga4' {
-//   export function initialize(measurementId: string | string[]): void;
-//   export function send(event: {
-//     hitType: string;
-//     page: string;
-//   }): void;
-// }
-
 declare module 'react-ga4' {
+
+  export function initialize(measurementId: string | string[]): void;
   export function initialize(measurementId: string | string[]): void;
 
   export function gtag(
@@ -14,4 +8,9 @@ declare module 'react-ga4' {
       eventNameOrConfig: string,
       params?: Record<string, string | number | boolean | undefined>
   ): void;
+
+  export function send(event: {
+    hitType: string;
+    page: string;
+  }): void;
 }
--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,23 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import AppRoutes from './AppRoutes/AppRoutes';
-import {useLocation} from "react-router-dom";
-import {initGA, trackPageView} from "../../data/analytics/analytics.ts";
 
 import styles from './App.module.scss'
 
 const App: React.FC = () => {
-
-  const location = useLocation();
-
-  useEffect(() => {
-    initGA(); // Инициализация GA один раз при загрузке
-  }, []);
-
-  useEffect(() => {
-    trackPageView(location.pathname + location.search); // Отправка события на каждый переход
-  }, [location]);
 
   return (
       <div className={styles.app}>
